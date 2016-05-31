3dfc162fd648c094abe8db02a47b37190a7fdf0c
hazan98 <hazanarnaud@gmail.com>
2016-05-31 10:26:18 +0700

    first commit

---
+++ app/src/androidTest/java/com/intentsfilters/RTest.java
@@ -0,0 +1,10 @@
+package com.intentsfilters;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by hazan on 31/05/16.
+ */
+public class RTest extends TestCase {
+
+}
---
+++ app/src/main/java/com/intentsfilters/MainActivity.java
@@ -0,0 +1,42 @@
+package com.intentsfilters;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/"));
+                startActivity(i);
+            }
+        });
+
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(android.content.Intent.ACTION_VIEW,Uri.parse("tel:089503029299"));
+                startActivity(i);
+            }
+        });
+    }
+
+}
